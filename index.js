49c135b708e93d327c754ddc8f3afec04c0f03a2
TinaTheDev91 <122223572+TinaTheDev91@users.noreply.github.com>
2023-05-13 21:26:58 -0400

    added inq quesrions

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'What is the description of your project?',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Please list your Table of Contents.',
+        name: 'toc'
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'What are the usage instructions?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Whom or what would you like to credit?',
+        name: 'credit'
+    },
+    {
+        type: 'input',
+        message: 'What license are you using?',
+        name: 'license'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
